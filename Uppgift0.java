eebac1cabcc299d52d309ccfe03682d42041a2f9
Ludvig-Rav <82057425+Ludvig-Rav@users.noreply.github.com>
2021-04-09 16:10:25 +0200

    HelloWorld-JavaCode

---
+++ Uppgift0.java
@@ -0,0 +1,40 @@
+
+public class Uppgift0
+{
+
+   public static void main(String[] args)
+   {
+
+//	   int x = ;
+//	   
+//	   int bonus1 = 0;
+//	   int bonus2 = 6;
+//	   int bonus3 = 10;
+//	   int bonus4 = 14;
+//	   int bonus5 = 18;
+//	   
+//	if(x <= 400) 
+//	   {
+//	   System.out.println("Bonusen är:" + bonus1 + "kr");
+//	   }  
+//	else if(x <= 800)
+//	   {
+//	   System.out.println("Bonusen är:" + bonus2 + "kr");
+//	   }
+//   else if(x <= 1200)
+//	{
+//	   System.out.println("Bonusen är:" + bonus3 + "kr");
+//	}
+//	else if(x <= 1600)
+//	{
+//	   System.out.println("Bonusen är:" + bonus4 + "kr");
+//	}
+//	else
+//	{
+//	   System.out.println("Bonusen är:" + bonus5 + "kr");
+//	}
+//
+      System.out.println("Hello world");
+   }
+
+}
